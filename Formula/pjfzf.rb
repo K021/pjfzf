4c04b90ff3774aaf56af8c7a4f9d827ef9e78d46
Jooeon Kim <joo2theeon@gmail.com>
2026-03-14 22:22:20 +0900

    fix: update Homebrew Formula URL and sha256

--- Formula/pjfzf.rb
+++ Formula/pjfzf.rb
@@ -1,8 +1,8 @@
 class Pjfzf < Formula
   desc "Project directory navigator with fuzzy finding"
-  homepage "https://github.com/kimjooeon/pjfzf"
-  url "https://github.com/kimjooeon/pjfzf/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 ""
+  homepage "https://github.com/K021/pjfzf"
+  url "https://github.com/K021/pjfzf/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "9d9d90f76a129c9a670b24be220eb6fe784753c46a738f42348600966d8d9893"
   license "MIT"
 
   depends_on "fzf"
